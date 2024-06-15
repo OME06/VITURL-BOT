3531403c53fc28d7962f9e9da68aee56636aa218
mahabub078 <m08771829@gmail.com>
2024-06-15 16:08:08 +0600

    Update .Hug2.js

--- scripts/commands/.Hug2.js
+++ scripts/commands/.Hug2.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-    name: "hugv3",
+    name: "married",
     version: "7.3.1",
     Permssion: 0,
     credits: "John Lester",
@@ -15,15 +15,14 @@ module.exports.config = {
     }
 };
 
-
 module.exports.onLoad = async() => {
     const { resolve } = global.nodemodule["path"];
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { downloadFile } = global.utils;
     const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'hugv2.png');
+    const path = resolve(__dirname, 'cache/canvas', 'marriedv02.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.ibb.co/zRdZJzG/1626342271-28-kartinkin-com-p-anime-obnimashki-v-posteli-anime-krasivo-30.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://i.ibb.co/mc9KNm1/1619885987-21-pibig-info-p-anime-romantika-svadba-anime-krasivo-24.jpg", path);
 }
 
 async function makeImage({ one, two }) {
@@ -33,7 +32,7 @@ async function makeImage({ one, two }) {
     const jimp = global.nodemodule["jimp"];
     const __root = path.resolve(__dirname, "cache", "canvas");
 
-    let batgiam_img = await jimp.read(__root + "/hugv2.png");
+    let batgiam_img = await jimp.read(__root + "/marriedv02.png");
     let pathImg = __root + `/batman${one}_${two}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
@@ -46,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(100, 100), 370, 40).composite(circleTwo.resize(100, 100), 330, 150);
+    batgiam_img.composite(circleOne.resize(100, 100), 55, 48).composite(circleTwo.resize(100, 100), 190, 40);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
