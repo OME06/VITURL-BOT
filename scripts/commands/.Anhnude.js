16745cb844b999138b120adf05e7f18326ee6a0d
mahabub078 <m08771829@gmail.com>
2024-06-16 19:10:54 +0600

    Create .Anhnude.js

---
+++ scripts/commands/.Anhnude.js
@@ -0,0 +1,31 @@
+module.exports.config = {
+  name: "anhnude",
+  version: "3.1.1",
+  permssion: 0,
+  prefix: true,
+  credits: "Nayan",
+  description: "Get fuck",
+  category: "img",
+  usages: "[@mention]",
+  cooldowns: 5,
+  dependencies: {
+      "axios": "",
+      "fs-extra": "",
+      "path": "",
+      "jimp": ""
+  }
+};
+
+module.exports.run = async ({ api, event }) => {
+	const axios = require('axios');
+	const request = require('request');
+	const fs = require("fs");
+	axios.get('https://apituandz1407.herokuapp.com/api/nude.php').then(res => {
+	let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
+	let callback = function () {
+					api.sendMessage({
+						attachment: fs.createReadStream(__dirname + `/cache/nobra.${ext}`)
+					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/nobra.${ext}`), event.messageID);
+				};
+				request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/nobra.${ext}`)).on("close", callback);
+			})
