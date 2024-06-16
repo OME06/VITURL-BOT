5f648fd14c0729985cfa91b25bbaa9b63964355f
mahabub078 <m08771829@gmail.com>
2024-06-16 18:09:13 +0600

    Update .Rankgif.js

--- scripts/commands/.Rankgif.js
+++ scripts/commands/.Rankgif.js
@@ -388,11 +388,11 @@ var min = Math.floor(Math.random() * 1);
   var data = await Currencies.getData(event.senderID);
   var exp =  data.exp;
   var money = data.money
-      if(money < 800) api.sendMessage("800$ dear friend",event.threadID,event.messageID)
+      if(money < 00) api.sendMessage("800$ dear friend",event.threadID,event.messageID)
           else {
-   Currencies.setData(event.senderID, options = {money: money - 800})
+   Currencies.setData(event.senderID, options = {money: money - 00})
    var callback = () => 
-   api.sendMessage({body:`Costs: 800$`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID);
+   api.sendMessage({body:`Costs: 00$`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID);
               api.setMessageReaction("✅", event.messageID, (err) => {}, true);
       return request(encodeURI(link[Math.floor(Math.random() * link.length)] + (max - min))).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
      }
